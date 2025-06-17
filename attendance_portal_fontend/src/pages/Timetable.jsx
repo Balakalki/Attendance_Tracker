e651173f4e68f761846a3c822a299467d7f03b81
Balakalki <aluribalakalki5@gmail.com>
2025-06-17 21:59:27 +0530

    build changes adding vercel.json

--- attendance_portal_fontend/src/pages/Timetable.jsx
+++ attendance_portal_fontend/src/pages/Timetable.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import TimetableConfig from "../components/timetable/TimetableConfig";
 import TimetableView from "../components/timetable/TimetableView";
 import {
   Tabs,
@@ -12,7 +11,6 @@ import ManageTimetable from "../components/timetable/MangageTimetable";
 import Loader from "../components/ui/loader";
 import { useNavigate } from "react-router-dom";
 
-<<<<<<< HEAD
 export default function Timetable() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -28,19 +26,6 @@ export default function Timetable() {
     } catch (err) {
       if (err.response && err.response.status === 401) {
         navigate("/login");
-=======
-export default function Timetable(){
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-    const fetchTimetable = async () => {
-      try {
-        const res = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/timetable`,{withCredentials: true});
-        setData(res.data);
-      } catch (err) {
-        setError(err.response.data.Error);
->>>>>>> e1bc8e5a3be178864aff6f0733033cc073d9a1d7
       }
       setLoading(false);
     }
@@ -48,8 +33,6 @@ export default function Timetable(){
   useEffect(() => {
     fetchTimetable();
   }, []);
-<<<<<<< HEAD
-  
 
   if (loading) {
     return (
@@ -75,22 +58,3 @@ export default function Timetable(){
     </div>
   );
 }
-=======
-  //  if (error) return <p>{error}</p>;
-  // if (!data) return <p>Loading...</p>;
-    return(
-        <div>
-            <Tabs defaultValue="viewTimetable">
-                <TabsList>
-                    <TabsTrigger value = "viewTimetable">View Timetable</TabsTrigger>
-                    <TabsTrigger value = "manageTimetable">Manage Timetable</TabsTrigger>
-                </TabsList>
-                <TabsContent value = "viewTimetable"><TimetableView data={data}/></TabsContent>
-                <TabsContent value = "manageTimetable">
-                    <TimetableConfig initialConfig={data}/>
-                </TabsContent>
-            </Tabs>
-        </div>
-    )
-}
->>>>>>> e1bc8e5a3be178864aff6f0733033cc073d9a1d7
